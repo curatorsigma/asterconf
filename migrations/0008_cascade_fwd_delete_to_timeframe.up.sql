83785f20ec1c2548045371e60dd84afdb532f95c
Jonathan Schleucher <mathnathan@icloud.com>
2025-08-22 14:02:51 +0200

    allow deletion of forwards with timeframes

---
+++ migrations/0008_cascade_fwd_delete_to_timeframe.up.sql
@@ -0,0 +1,3 @@
+-- Add ON DELETE CASCADE to map_call_forward_timeframe FK to call_forward
+ALTER TABLE map_call_forward_timeframe DROP CONSTRAINT map_call_forward_timeframe_fwd_id_fkey, ADD  CONSTRAINT map_call_forward_timeframe_fwd_id_fkey FOREIGN KEY (fwd_id) REFERENCES call_forward(fwd_id) ON DELETE CASCADE;
+
